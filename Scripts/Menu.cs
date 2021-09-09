faa8dc1bf1e17c3e64ea46d85c7a6e05e66c0431
Solidlool <50490091+Solidlool@users.noreply.github.com>
2021-09-09 14:28:30 +0100

    Scripts Uploaded

---
+++ Scripts/Menu.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Menu : MonoBehaviourPunCallbacks
+{
+
+    [Header("Screens")]
+    public GameObject mainScreen;
+    public GameObject lobbyScreen;
+
+    [Header("Main Screen")]
+    public Button createRoomButton;
+    public Button joinRoomButton;
+
+    [Header("Lobby Screen")]
+    public TextMeshProUGUI playerListText;
+    public Button startGameButton;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createRoomButton.interactable = false;
+        joinRoomButton.interactable = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        createRoomButton.interactable = true;
+        joinRoomButton.interactable = true;
+    }
+
+    void SetScreen(GameObject screen)
+    {
+        //deactivate all screens
+        mainScreen.SetActive(false);
+        lobbyScreen.SetActive(false);
+
+        //enable selected screen
+        screen.SetActive(true);
+    }
+
+    public void OnCreateRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.CreateRoom(roomNameInput.text);
+    }
+
+    public void OnJoinRoomButton(TMP_InputField roomNameInput)
+    {
+        NetworkManager.instance.JoinRoom(roomNameInput.text);
+    }
+
+    public void OnPlayerNameUpdate(TMP_InputField playerNameInput)
+    {
+        PhotonNetwork.NickName = playerNameInput.text;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SetScreen(lobbyScreen);
+
+        photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        // we dont RPC it because OnPlayerLeftRoom gets called for all clients in the room
+        UpdateLobbyUI();
+    }
+
+    //updates the lobby UI to show player list and host buttons
+    [PunRPC]
+    public void UpdateLobbyUI()
+    {
+        playerListText.text = "";
+
+        //displayer all the player currently in the lobby
+        foreach(Player player in PhotonNetwork.PlayerList)
+        {
+            playerListText.text += player.NickName + "\n";
+
+        }
+
+        // only the  host can start the game
+        if (PhotonNetwork.IsMasterClient)
+     
+            startGameButton.interactable = true;
+            else
+            startGameButton.interactable = false;
+           
+    }
+
+    public void OnLeaveLobbyButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SetScreen(mainScreen);
+    }
+
+    public void OnStartGameButton()
+    {
+        NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
+    }
+
+}
